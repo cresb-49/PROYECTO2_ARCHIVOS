dba27fd619772685e3bc66e3fd8592ead3f5b56c
cresb-49 <carlosbpac@gmail.com>
2023-04-14 19:08:49 -0600

    header, footer app

--- FRONTEND/ecomercegt/src/App.vue
+++ FRONTEND/ecomercegt/src/App.vue
@@ -1,25 +1,39 @@
 <template>
+  <HeaderApp></HeaderApp>
+  
   <div>
     <h1>Hola mundo</h1>
     <button class="btn red" @click="saludar" >Hola <i class="material-icons">save</i> </button>
+    <div>{{ info }}</div>
   </div>
+  <FooterApp></FooterApp>
 </template>
 
 <script>
 //Sector de codigo js
 import M from 'materialize-css';
-
+import HeaderApp from "./components/HeaderComponent.vue";
+import FooterApp from "./components/FooterComponent.vue";
 
 export default {
   name: 'App',
+  components:{
+    HeaderApp,
+    FooterApp
+  },
   mounted(){
     this.axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(()=>{})
+    .then(response=>(this.info=response))
   },
   methods:{
     saludar()
     {
       M.toast({html:'Hola mundo'});
+    }
+  },
+  data(){
+    return{
+      info:null
     }
   }
 }
---
+++ FRONTEND/ecomercegt/src/components/HeaderComponent.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo">Logo</a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
+                    <li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'HeaderApp',
+    props: {
+        // Propiedades del componente
+    },
+    data() {
+        return {
+            // Variables de estado del componente
+        }
+    },
+    methods: {
+        // Métodos del componente
+    },
+    mounted() {
+        // Código que se ejecuta cuando el componente se monta en la página
+    }
+}
+</script>
+  
+<style>
+/* Estilos CSS del componente */
+</style>
+  
