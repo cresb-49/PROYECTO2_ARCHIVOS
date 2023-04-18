1fa20ca759349c2e896a99e9752f1ca82a4caf16
cresb-49 <carlosbpac@gmail.com>
2023-04-18 01:38:58 -0600

    estilo del registro de admin

--- FRONTEND/ecomercegt/src/components/RegistroAdministradorComponent.vue
+++ FRONTEND/ecomercegt/src/components/RegistroAdministradorComponent.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-        <SingUpComponent type-role="ADMIN" ></SingUpComponent>
+    <div class="container">
+        <div class="sombra">
+            <SingUpComponent type-role="ADMIN" ></SingUpComponent>
+        </div>
     </div>
 </template>
 
@@ -19,3 +21,49 @@ export default{
     }
 }
 </script>
+
+<style scoped>
+.container{
+    width:100%;
+	margin:auto;
+	max-width:400px;
+	position:relative;    
+}
+
+.sombra{
+    border-radius: 5px;
+    box-shadow:0 0px 0px 0 rgba(0,0,0,.24),0 17px 50px 0 rgba(0,0,0,.19);
+}
+
+.tabs-container {
+    display: flex;
+}
+
+.tabs-item {
+    width: 100%;
+    text-align: center;
+    padding: 8px;
+    cursor: pointer;
+    background-color: transparent;
+    color:#343a40;
+    
+}
+
+.tab-login{
+    border-radius: 5px 0px 0px 0px;
+    border-color:  	#d0d2d4;
+    border-width: 1px 1px 0px 1px;
+    border-style: solid;
+}
+.tab-sign{
+    border-radius: 0px 5px 0px 0px;
+    border-color:  	#d0d2d4;
+    border-width: 1px 1px 0px 0px;
+    border-style: solid;
+}
+
+.tabs-item-active,
+.tabs-item:hover {
+    background-color:#007bff;
+    color:#FFFFFF;
+}</style>
